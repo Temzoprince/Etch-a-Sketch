0cda0456157920609ad804e7012c48c545024968
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-25 17:08:06 +0000

    Create 4 columns
    I can make each column a flex container consisting of 4 flex items
    This means that instead of generating 256 divs, I can just make 16
    column divs and 16 row divs inside each column
    More efficient code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container')
 
 // 256 elements in 16x16 grid
-for (i = 1; i <= 256; i++) {
+for (i = 1; i <= 4; i++) {
     let div = document.createElement('div')
     div.classList.add(`div${i}`)
     container.appendChild(div);
