713ca587f971536a3f371aac3eb1b8ec645b24ec
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-25 17:44:27 +0000

    Create a grid with as many rows as columns
    Following from previous two commits, this code includes the html, css
    and dom manipulation for a grid of equal size.
    Equal size meaning it has the same number of columns as there are rows
    You can change the how big or small the grid is by changing the variable
    i in the for loop

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const container = document.querySelector('.container')
 
-// 256 elements in 16x16 grid
+/**
+ * Create a grid of any size
+ * This grid has equal rows and columns
+ * To change the size of the grid, just change the variable [i] in the columns section
+ */
+
+// Columns section - Create the number of columns needed for the grid
 for (i = 1; i <= 4; i++) {
     let div = document.createElement('div')
     div.classList.add(`col${i}`)
     container.appendChild(div);
 }
 
-// For each column, make as many rows as there are columns
+// Rows Section - For each column, make as many rows as there are columns
 const columns = document.querySelectorAll('div')
 
-//for each column
+// In each column, make as many rows as there are columns
 for (i = 1; i < columns.length; i++) {
+    // Select each column
     let currentDiv = document.querySelector(`.col${i}`)
     console.log(`current column is col: ${i}`)
     for (j = 1; j < columns.length; j++) {
-        // create as many divs as there columns
+        // Create as many rows as there are columns
         console.log(`current row is row: ${j}`)
         let div = document.createElement('div')
         div.classList.add(`row${j}`)
