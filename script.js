6573a4ace7b7bc71546863fee90b2bdb1f854307
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-25 20:09:22 +0000

    Create grid function
    Put the code to create a grid in a function
    Increases is reusability and cleans it up

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
 /**
  * Create a grid of any size
  * This grid has equal rows and columns
  * To change the size of the grid, just change the variable [i] in the columns section
+ * Hovering over each grid changes background color
  */
 
-// Columns section - Create the number of columns needed for the grid
-for (i = 1; i <= 16; i++) {
-    let div = document.createElement('div')
-    div.classList.add(`col${i}`)
+function createGrid(gridSize) {
+  // Columns section - Create the number of columns needed for the grid
+  for (i = 1; i <= gridSize; i++) {
+    let div = document.createElement("div");
+    div.classList.add(`col${i}`);
     container.appendChild(div);
-}
+  }
 
-// Rows Section - For each column, make as many rows as there are columns
-const columns = document.querySelectorAll('div')
+  // Rows Section - For each column, make as many rows as there are columns
+  const columns = document.querySelectorAll("div");
 
-// In each column, make as many rows as there are columns
-for (i = 1; i < columns.length; i++) {
+  // In each column, make as many rows as there are columns
+  for (i = 1; i < columns.length; i++) {
     // Select each column
-    let currentDiv = document.querySelector(`.col${i}`)
-    console.log(`current column is col: ${i}`)
+    let currentDiv = document.querySelector(`.col${i}`);
+    console.log(`current column is col: ${i}`);
     for (j = 1; j < columns.length; j++) {
-        // Create as many rows as there are columns
-        console.log(`current row is row: ${j}`)
-        let div = document.createElement('div')
-        div.classList.add(`row${j}`)
-        console.log(currentDiv)
-        currentDiv.appendChild(div);
+      // Create as many rows as there are columns
+    //   console.log(`current row is row: ${j}`);
+      let div = document.createElement("div");
+      div.classList.add(`row${j}`);
+    //   console.log(currentDiv);
+      currentDiv.appendChild(div);
     }
-    
+  }
 }
 
+createGrid(16);
+
+const columns = document.querySelectorAll(".container > *");
+console.log(columns.length)
 
 // Div changes background color upon hover state
 for (i = 1; i < columns.length; i++) {
-
-    currentColumn = document.querySelector(`.col${i}`)
-    for (j = 1; j < columns.length; j++) {
-        let currentCell = currentColumn.querySelector(`.row${j}`)
-        // Hovering over each div change background color
-        currentCell.addEventListener("mouseenter", (e) => {
-            console.log(e)
-            currentCell.style.background = "#555555";
-        })
-    }
-    
+  currentColumn = document.querySelector(`.col${i}`);
+  for (j = 1; j < columns.length; j++) {
+    let currentCell = currentColumn.querySelector(`.row${j}`);
+    // Hovering over each div change background color
+    currentCell.addEventListener("mouseenter", (e) => {
+      console.log(e);
+      currentCell.style.background = "#555555";
+    });
+  }
 }
