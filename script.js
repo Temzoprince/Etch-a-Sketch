47ac3cbf8bf3c8d414e2d9ca07897a27890278c4
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-25 17:32:32 +0000

    Create as many rows as there are columns in grid
    Instead of making 256 cells in a for loop, a nested for loop was used
    to automatically create as many rows as there are columns
    This makes the code cleaner and reusable in another situation where
    a grid of equal height and width of any size is needed

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const container = document.querySelector('.container')
 // 256 elements in 16x16 grid
 for (i = 1; i <= 4; i++) {
     let div = document.createElement('div')
-    div.classList.add(`div${i}`)
+    div.classList.add(`col${i}`)
     container.appendChild(div);
+}
+
+// For each column, make as many rows as there are columns
+const columns = document.querySelectorAll('div')
+
+//for each column
+for (i = 1; i < columns.length; i++) {
+    let currentDiv = document.querySelector(`.col${i}`)
+    console.log(`current column is col: ${i}`)
+    for (j = 1; j < columns.length; j++) {
+        // create as many divs as there columns
+        console.log(`current row is row: ${j}`)
+        let div = document.createElement('div')
+        div.classList.add(`row${j}`)
+        console.log(currentDiv)
+        currentDiv.appendChild(div);
+    }
+    
 }
